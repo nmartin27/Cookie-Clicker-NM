a1b2720260a1eb40d5f9710ee3f4ec99b01e3640
nmartin <nate.martin27@gmail.com>
2024-11-14 10:23:36 -0500

    port change

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-console.log('Listening on port 3000');
+app.listen(3001, () => {
+console.log('Listening on port 3001');
 });
